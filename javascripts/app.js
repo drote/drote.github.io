a60c9edd2cb47d98b8fdd9a5365d5aead4a93396
drote <drote.email@gmail.com>
2019-08-21 22:49:57 -0700

    tablet css

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,9 +3,11 @@ $(function() {
 		if (window.scrollY > 5) {
 			$('header a').addClass('smaller');
 			$('h1').addClass('smaller');
+			$('nav').addClass('smaller');
 		} else {
 			$('h1').removeClass('smaller');
 			$('header a').removeClass('smaller');
+			$('nav').removeClass('smaller');
 		}
 	}
 
